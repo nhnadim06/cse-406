38b6d772a9811d71ce5cf0078eb153e09eeeef15
nhnadim06 <nhnadim06@gmail.com>
2025-08-22 21:33:45 +0600

    FCFS_DiskScheduling.py

--- FCFS_DiskScheduling.py
+++ FCFS_DiskScheduling.py
@@ -1,23 +1,22 @@
-def FCFS(request, head):
+def FCFS(requests, head):
     total_head_movement = 0
     c_head = head
     order = []
 
-    for i in request:
-        if i == c_head: 
-            continue
-        else:
-            distance = abs(i - c_head)
-            total_head_movement += distance
-            order.append(i)
-            c_head = i
+    requests = [r for r in requests if r != head]
+
+    for i in requests:
+        distance = abs(i - c_head)
+        total_head_movement += distance
+        order.append(i)
+        c_head = i
 
     return total_head_movement, order
 
 
-request = list(map(int, input("Input requests: ").split()))
+requests = list(map(int, input("Input requests: ").split()))
 head = int(input("Input head number: "))
 
-total_movement, order = FCFS(request, head)
+total_movement, order = FCFS(requests, head)
 print("\nOrder of execution:", order)
 print("Total head movement:", total_movement)
